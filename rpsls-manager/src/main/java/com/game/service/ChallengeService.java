6a667049175a1d6d912374567ab0e316249c97e0
ra-kus <rakus.sebastian@gmail.com>
2018-02-23 02:13:45 +0100

    Revert "rpsls game"
    This reverts commit ec206adc606b1b66754f0ee4174b9f8704d7a4b3.

--- rpsls-manager/src/main/java/com/game/service/ChallengeService.java
+++
@@ -1,9 +0,0 @@
-package com.game.service;
-
-import java.util.List;
-
-import com.game.dto.ContestDTO;
-
-public interface ChallengeService {
-	public String challangePlayers(List<ContestDTO> players);
-}
